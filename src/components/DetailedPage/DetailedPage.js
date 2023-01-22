559ef4086e15581fc5b6cba32aae47a8f16a4736
evgeniy <jon666gridin@yandex.ru>
2023-01-22 21:19:36 +0500

    fix

--- src/components/DetailedPage/DetailedPage.js
+++ src/components/DetailedPage/DetailedPage.js
@@ -4,17 +4,14 @@ import { Link } from "react-router-dom";
 import { pictures } from "../../utils/Api";
 
 function DetailedPage(props) {
-  console.log(props);
+
+  console.log(props)
   return (
     <section className="detailed-page">
-      <Link to="/" className="detailed-page__btn">
-        Go Back
-      </Link>
+      <Link to='/' className="detailed-page__btn">Go Back</Link>
       <img className="detailed-page__img" src={pictures} alt="картинка" />
       <div className="detailed-page__container">
-        <h2 className="detailed-page__title">
-          {props.card.title[0].toUpperCase() + props.card.title.slice(1)}
-        </h2>
+        <h2 className="detailed-page__title">{props.card.title[0].toUpperCase() + props.card.title.slice(1)}</h2>
         <p className="detailed-page__autor">Author: {props.card.name}</p>
         <p className="detailed-page__text">{props.card.body}</p>
       </div>
